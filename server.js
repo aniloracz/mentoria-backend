2b8e28042040f84405b6fb982b936e22563e17fb
AlejandroDojo <ultiimately@gmail.com>
2024-07-03 11:10:35 -0400

    Ajustes dia miercoles, algo funcional con bugs

--- server.js
+++ server.js
@@ -1,15 +1,18 @@
 const express = require("express");
 const cors = require("cors");
-const RoutesJugador = require('./routes/jugador.routes')
+const RoutesJugador = require("./routes/jugador.routes");
 const app = express();
-const socketIo= require('socket.io')
-const server= require('http').createServer(app)
-const io= socketIo(server, { cors: { origin: '*' } })
+const socketIo = require("socket.io");
+const http = require("http");
+const server = http.createServer(app);
+const io = require("socket.io")(server, {
+  cors: {
+    origin: "*",
+  },
+});
 
 //requerimos la base de datos
-require('./server/config/baseDato');
-
-
+require("./config/mongoose.config");
 
 //Middleware
 app.use(cors());
@@ -18,16 +21,64 @@ app.use(express.urlencoded({ extended: true }));
 //le pasamos la app a nuestra ruta
 RoutesJugador(app);
 
-
-
-
-
-server.listen(8080, () => {
+server.listen(5000, () => {
   console.log("Activo en el puerto 8080");
 });
 
+let jugadoresConectados = 0;
+let jugador1Name = "";
+let jugador2Name = "";
+let jugador1socket ;
+let jugador2socket ;
 
-io.on('connection', (socket)=> {
-  console.log("Se conecto un usuario", socket)
-  socket.broadcast.emit("Anuncio", "Llegó alguien mas")
+io.on("connection", (socket) => {
+  console.log("Se conecto un Jugador", socket.id);
+  jugadoresConectados++;
+
+  if (!jugador1socket) {
+    jugador1socket = socket;
+    jugador1socket.emit("asignaJugador", 1);
+  } else if (!jugador2socket) {
+    jugador2socket = socket;
+    jugador2socket.emit("asignaJugador", 2);
+  } else {
+    io.to(socket).emit("EsperarTurno", "Pestañeaste, espera tu turno.");
+  }
+  if (jugador1socket) {
+    jugador1socket.on("JugadorNombre", (jugadorNombre) => {
+      jugador1Name = jugadorNombre;
+      jugador1socket.emit('EsperandoContrincante', `!Bienvenido ${jugadorNombre}, espera tu retador!.`)
+    });
+  }
+  if (jugador2socket) {
+    jugador2socket.on("JugadorNombre", (jugadorNombre) => {
+      jugador2Name = jugadorNombre;
+      socketJugador1.emit("RetadorAsignado", `Tu retador es ${jugador2Name}`);
+      socketJugador2.emit("RetadorAsignado", `Tu retador es ${jugador1Name}`);
+    });
+  }
+
+    socket.on("disconnect", (reason) => {
+
+    if (socket === jugador1socket) {
+      console.log("Se ha desconectado el jugador 1: "+ jugador1Name);
+      if (jugador2socket) {
+        jugador2socket.emit("contrincanteRetirado", `!Tu contrincante ${jugador1Name} se meo, se asustó! Ganas por desconexión`);
+      }
+      jugador1socket = "";
+      jugador1Name = "";
+      jugadoresConectados--;
+    } else if (socket === jugador2socket) {
+      console.log("Se ha desconectado el jugador 2: "+ jugador2Name);
+      if (jugador1socket) {
+        jugador1socket.emit("contrincanteRetirado", `!Tu contrincante ${jugador2Name} se meo, se asustó! Ganas por desconexión`);
+      }
+      jugador2socket = "";
+      jugador2Name = "";
+      jugadoresConectados--;
+    } else {
+      console.log("Se desconecto alguien de la sala de espera. " + socket.id);
+    }
+    
+  });
 });
