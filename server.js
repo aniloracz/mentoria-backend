05bdfa5ae4d76ba52656d4a9b16ea36899d107a9
aniloracz <aniloracz@gmail.com>
2024-07-08 12:16:06 -0300

    socket io fixed

--- server.js
+++ server.js
@@ -26,54 +26,59 @@ server.listen(5000, () => {
 });
 
 let jugadoresConectados = 0;
-let jugador1Name = "";
-let jugador2Name = "";
-let jugador1socket ;
-let jugador2socket ;
+let jugador1Name  ;
+let jugador2Name  ;
+let jugador1Socket ;
+let jugador2Socket ;
 
 io.on("connection", (socket) => {
   console.log("Se conecto un Jugador", socket.id);
   jugadoresConectados++;
 
-  if (!jugador1socket) {
-    jugador1socket = socket;
-    jugador1socket.emit("asignaJugador", 1);
-  } else if (!jugador2socket) {
-    jugador2socket = socket;
-    jugador2socket.emit("asignaJugador", 2);
+  if (!jugador1Socket) {
+    console.log("entró a jugador 1 vacío")
+    jugador1Socket = socket;
+    jugador1Socket.emit("asignaJugador", 1);
+  } else if (!jugador2Socket) {
+    console.log("entró a jugador 2 vacío")
+    jugador2Socket = socket;
+    jugador2Socket.emit("asignaJugador", 2);
   } else {
     io.to(socket).emit("EsperarTurno", "Pestañeaste, espera tu turno.");
+    console.log("entró a jugador sala de espera")
   }
-  if (jugador1socket) {
-    jugador1socket.on("JugadorNombre", (jugadorNombre) => {
+  if (jugador1Socket) {
+    jugador1Socket.on("JugadorNombre", (jugadorNombre) => {
       jugador1Name = jugadorNombre;
-      jugador1socket.emit('EsperandoContrincante', `!Bienvenido ${jugadorNombre}, espera tu retador!.`)
+      jugador1Socket.emit('EsperandoContrincante', `!Bienvenido ${jugadorNombre}, espera tu retador!.`)
     });
   }
-  if (jugador2socket) {
-    jugador2socket.on("JugadorNombre", (jugadorNombre) => {
+  if (jugador2Socket) {
+    jugador2Socket.on("JugadorNombre", (jugadorNombre) => {
+      console.log("llega nombre del front" +jugadorNombre)
       jugador2Name = jugadorNombre;
-      socketJugador1.emit("RetadorAsignado", `Tu retador es ${jugador2Name}`);
-      socketJugador2.emit("RetadorAsignado", `Tu retador es ${jugador1Name}`);
+
+      jugador1Socket? jugador1Socket.emit("RetadorAsignado", `Tu retador es ${jugador2Name}`) : null;
+      jugador2Socket.emit("RetadorAsignado", `Tu retador es ${jugador1Name}`);
     });
   }
 
     socket.on("disconnect", (reason) => {
 
-    if (socket === jugador1socket) {
+    if (socket === jugador1Socket) {
       console.log("Se ha desconectado el jugador 1: "+ jugador1Name);
-      if (jugador2socket) {
-        jugador2socket.emit("contrincanteRetirado", `!Tu contrincante ${jugador1Name} se meo, se asustó! Ganas por desconexión`);
+      if (jugador2Socket) {
+        jugador2Socket.emit("contrincanteRetirado", `!Tu contrincante ${jugador1Name} se meo, se asustó! Ganas por desconexión`);
       }
-      jugador1socket = "";
+      jugador1Socket = "";
       jugador1Name = "";
       jugadoresConectados--;
-    } else if (socket === jugador2socket) {
+    } else if (socket === jugador2Socket) {
       console.log("Se ha desconectado el jugador 2: "+ jugador2Name);
-      if (jugador1socket) {
-        jugador1socket.emit("contrincanteRetirado", `!Tu contrincante ${jugador2Name} se meo, se asustó! Ganas por desconexión`);
+      if (jugador1Socket) {
+        jugador1Socket.emit("contrincanteRetirado", `!Tu contrincante ${jugador2Name} se meo, se asustó! Ganas por desconexión`);
       }
-      jugador2socket = "";
+      jugador2Socket = "";
       jugador2Name = "";
       jugadoresConectados--;
     } else {
